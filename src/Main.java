08195068f73cf554a592f942034f7470d4962221
apcompscimiller <bths@gmail.com>
2025-11-20 10:09:01 -0500

    blud

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello eworld");
         System.out.println(" evil hello world");
-        System.out.println("blud");
+        System.out.println("bludasdasdasdsadasdasdasddHELP");
     }
 }
---
+++ src/chud.java
@@ -0,0 +1,6 @@
+public class chud
+{
+    public static void main(String[] args) {
+        System.out.println("tu9os 9s cjhud class");
+    }
+}
